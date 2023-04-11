15a5376f906b6f45de2013bc24a9b0de72cc1ea4
lugfigue <lugfigue@bancolombia.com.co>
2023-04-11 12:04:33 -0500

    before begin NgRx Data

--- src/app/courses/course.effects.ts
+++ src/app/courses/course.effects.ts
@@ -14,6 +14,21 @@ export class CourseEffects {
     );
   });
 
+  updateCourseAnOptimisticWay$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(CourseActions.courseUpdated),
+        concatMap((action) =>
+          this.coursesHttpService.saveCourse(
+            action.update.id,
+            action.update.changes
+          )
+        )
+      );
+    },
+    { dispatch: false }
+  );
+
   constructor(
     private actions$: Actions,
     private coursesHttpService: CoursesHttpService
